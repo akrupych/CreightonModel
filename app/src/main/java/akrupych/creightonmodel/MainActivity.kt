618798eb3b596a25675e2e54c7b76d9b191e5c9e
akrupych <akrupych@remit.se>
2018-02-11 16:26:57 +0200

    blood recording

--- app/src/main/java/akrupych/creightonmodel/MainActivity.kt
+++ app/src/main/java/akrupych/creightonmodel/MainActivity.kt
@@ -2,11 +2,19 @@ package akrupych.creightonmodel
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bloodDropdown.setOnClickListener { toggleBloodDropdown() }
+    }
+
+    private fun toggleBloodDropdown() {
+        val shouldOpen = !bloodDropdown.isSelected
+        bloodDropdown.isSelected = shouldOpen
+        bloodSection.setVisible(shouldOpen)
     }
 }
